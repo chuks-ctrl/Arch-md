4b77b5c7e53b91aa7c1bea4ae3e1e2adfef96606
chuks-ctrl <chuksnbony@gmail.com>
2025-01-02 07:05:39 +0100

    Update config.js

--- config.js
+++ config.js
@@ -8,8 +8,8 @@ const chalk = require("chalk")
 const fs = require("fs")
 
 //======= Change settings =======\\
-global.owner = ["2347036214381"]
-global.botname = ["Arch Md"]
+global.owner = ["2349058573611"]
+global.botname = ["𝕾𝖎𝖌𝖒𝖆"]
 global.simbol = "♘"
 global.typeMenu = 'v5' //don't change 
 global.anticall = false // set true to turn anticall always on
